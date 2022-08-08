958869d02196f2acae1d3fd8eca0a2c7134bba4b
KSVoss <Kai.S.Voss@t-online.de>
2022-08-08 15:35:05 +0200

    start with test

---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class IntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void expectUnauthorized() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/hello")
+        ).andExpect(
+                status().isUnauthorized()
+        );
+    }
+    }
+
